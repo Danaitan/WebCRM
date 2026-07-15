2e80668c13b06adc68a361835a2b4a6ffa8784ce
MICROLEASING\100664 <danaitan@microleasingplc.com>
2026-07-15 16:52:54 +0700

    no message

--- webCRM/Program.cs
+++ webCRM/Program.cs
@@ -26,12 +26,8 @@ app.UseRouting();
 app.UseSession();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();

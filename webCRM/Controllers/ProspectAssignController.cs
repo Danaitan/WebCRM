3dce9ef8834dc8a7e1ce1105db31c06bf4400c5c
MICROLEASING\100664 <danaitan@microleasingplc.com>
2026-07-20 08:43:33 +0700

    ใส่ API คร่าวๆ

--- webCRM/Controllers/ProspectAssignController.cs
+++ webCRM/Controllers/ProspectAssignController.cs
@@ -8,11 +8,81 @@ using System.Text.Json;
 
 namespace webCRM.Controllers
 {
-    public class ProspectAssignController : Controller
+    public class ProspectAssignController(IConfiguration configuration) : Controller
     {
+        string? bearerToken = Environment.GetEnvironmentVariable("ApiSettings_BearerToken") ?? configuration["ApiSettings:BearerToken"];
+        string? domain = Environment.GetEnvironmentVariable("ApiSettings_APIDomain") ?? configuration["ApiSettings:APIDomain"];
+
         public async Task<IActionResult> Index()
         {
             return View("prospectAssign");
+        }
+
+        public async Task<List<ProductGet>> GetCampainList(string userId)
+        {
+
+            try
+            {
+                var handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+                };
+                using (var client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                    string getCase = "ownerview";
+                    var response = await client.GetAsync($"{domain}/crm/api/v1/p2/getProducts/{userId}/{getCase}");
+                    response.EnsureSuccessStatusCode();
+                    string data = await response.Content.ReadAsStringAsync();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = System.Text.Json.JsonSerializer.Deserialize<List<ProductGet>>(data, new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                        var result = apiResponse;
+
+                        return result ?? new List<ProductGet>();
+                    }
+
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.ErrorMessage = "เกิดข้อผิดพลาดในการโหลดข้อมูล: " + ex.Message;
+                return new List<ProductGet>();
+            }
+
+            return new List<ProductGet>();
+
+        }
+
+        public async Task<string> GetBatchList(string productCode)
+        {
+
+            try
+            {
+                var handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+                };
+                using (var client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                    var response = await client.GetAsync($"{domain}/crm/api/v1/p2/getProductBatch/{productCode}/-/-");
+                    response.EnsureSuccessStatusCode();
+
+                    string data = await response.Content.ReadAsStringAsync();
+
+                    return data;
+
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.ErrorMessage = "เกิดข้อผิดพลาดในการโหลดข้อมูล: " + ex.Message;
+                return "";
+            }
+
         }
 
     }
